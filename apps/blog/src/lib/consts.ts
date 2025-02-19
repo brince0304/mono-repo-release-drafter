a2b6d5cb714417ad47ba2bdd3c335110f73936f2
brince0304 <brince0304@gmail.com>
2025-02-19 10:52:24 +0900

    test: deploy-test-5

--- apps/blog/src/lib/consts.ts
+++ apps/blog/src/lib/consts.ts
@@ -3,6 +3,6 @@ export const BLOG_DESCRIPTION =
   '주니어 프론트엔드 개발자 백석현의 개발 블로그입니다. 제 삶을 기록하고, 개발 관련 정보를 공유합니다. :)';
 export const BLOG_TITLE = 'BRINCE - 프론트엔드 개발 블로그';
 export const BLOG_KEYWORDS = '프론트엔드, 개발, 블로그, Next.js, React, TailwindCSS';
-export const BLOG_AUTHOR = '백석현2';
+export const BLOG_AUTHOR = '백석현22';
 export const BLOG_IMAGE_URL =
   'https://avatars.githubusercontent.com/u/110673427?s=400&u=b32c9bbb7d88e6c54f4e7dca4a3c2a2ec24c721c&v=4';
